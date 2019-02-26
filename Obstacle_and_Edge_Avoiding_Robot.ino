9b73bf97ff70c0692fb8e09bd12c9f11d7b61fe8
Aonmoy <aonmoy0.0@gmail.com>
2019-02-26 11:29:44 +0600

    ADD_StopandStartFunction

--- Obstacle_and_Edge_Avoiding_Robot.ino
+++ Obstacle_and_Edge_Avoiding_Robot.ino
@@ -1,43 +1,58 @@
 const int leftForward = 2;
 const int leftBackward = 3;
-const int rightForward = 5;
-const int rightBackward = 4;
+const int rightForward = 4;
+const int rightBackward = 5;
+const int enable_12 = 8;
+const int enable_34 = 10;
 
-
-void setup() 
+void setup()
 {
+  pinMode(enable_12, OUTPUT);
+  pinMode(enable_34, OUTPUT);
   pinMode(leftForward , OUTPUT);
   pinMode(leftBackward , OUTPUT);
-  
-
+  pinMode(rightForward, OUTPUT);
+  pinMode(rightBackward, OUTPUT);
 }
 
 void loop()
 {
-  
+  motorStart();
   goForward();
- 
-  
-  
- 
+  delay(10000);
+  motorStop();
+  delay(3000);
+  motorStart();
+  goBackward();
+  delay(10000);
+  motorStop();
+  delay(3000);
+
+
+
+
+
 }
 void goForward() {
-  digitalWrite(leftForward , LOW);
-  digitalWrite(leftBackward, HIGH);
-  digitalWrite(rightForward,HIGH);
-  digitalWrite(rightBackward, LOW);
-  
-}
-
-void goBackward() {
-  digitalWrite(leftForward, HIGH);
+  digitalWrite(leftForward , HIGH);
   digitalWrite(leftBackward, LOW);
   digitalWrite(rightForward, HIGH);
   digitalWrite(rightBackward, LOW);
+
+}
+
+void goBackward() {
+  digitalWrite(leftForward, LOW);
+  digitalWrite(leftBackward, HIGH);
+  digitalWrite(rightForward, LOW);
+  digitalWrite(rightBackward, HIGH);
+}
+
+void motorStart() {
+  digitalWrite(enable_12, HIGH);
+  digitalWrite(enable_34, HIGH);
 }
 void motorStop() {
-  digitalWrite(leftForward, LOW);
-  digitalWrite(leftBackward, LOW);
-  digitalWrite(rightForward, LOW);
-  digitalWrite(rightBackward, LOW);
+  digitalWrite(enable_12, LOW);
+  digitalWrite(enable_34, LOW);
 }
